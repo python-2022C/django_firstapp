b76bf97f0d553cf53ee82244eddfacf750140655
sanjarbek <sanjarsaidov173@gmail.com>
2022-10-02 12:54:11 +0500

    first homework in django

---
+++ README.md
@@ -0,0 +1,10 @@
+# Practise makes perfect
+
+## Create views that do these simple arithmetics
+## on values that sent from postman
+- adds 
+- subtracts
+- multiple
+- division
+
+### then of course get creative!!!
---
+++ arithmetics_app/views.py
@@ -0,0 +1,15 @@
+from django.http import JsonResponse
+import json
+
+
+def home(request):
+    # this data comes as bytestring
+    # that is why we havte decode to string
+    byte = request.body
+    # this decote bytestring do string
+    text = byte.decode()
+    # since we need dict object 
+    # we have to turn string to dict object
+    data = json.loads(text)
+    # returns sum of dict object values
+    return JsonResponse({'sum': int(data.get('a', 0))+int(data.get('b', 0))})
