fde0e30ca651a1ed51624c1134823e4e5931c1b5
878 <majidovdiyorbek741@gmail.com>
2022-10-03 16:06:32 +0500

    myproject

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from arithmetics_app import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('subtracts/', views.subtracts),
+    path("division/", views.division),
+    path("multiple/", views.multiple)
 ]
